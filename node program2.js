97788674076d973a6957d1254e50bba542b9d360
Gautamdev20 <gautamdevansh07@gmail.com>
2024-09-30 12:50:20 +0530

    Create node program2.js

---
+++ node program2.js
@@ -0,0 +1,26 @@
+function romanToInt(s) {
+    const roman = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    };
+
+    let total = 0;
+    
+    for (let i = 0; i < s.length; i++) {
+        const current = roman[s[i]];
+        const next = roman[s[i + 1]];
+
+        if (next > current) {
+            total -= current;
+        } else {
+            total += current;
+        }
+    }
+
+    return total;
+}
